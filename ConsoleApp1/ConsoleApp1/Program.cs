5b8405d942d4d5fc7e15049a51573e41a11c4220
door-sniffer <mkkop1@gmail.com>
2024-01-31 10:35:05 -0500

    paint ball gun app

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintBallApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaintballGun gun = new PaintballGun();
+
+            while (true)
+            {
+                Console.WriteLine($"{gun.Balls} balls, {gun.GetBallsLoaded()} loaded");
+                if (gun.IsEmpty()) Console.WriteLine("WARNING: You're out of ammo");
+                Console.WriteLine("Space to shoot, r to reload, + to add ammo, q to quit");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == ' ') Console.WriteLine($"Shooting returned {gun.Shoot()}");
+                else if (key == 'r') gun.Reload();
+                else if (key == '+') gun.Balls += PaintballGun.MAGAZINE_SIZE;
+                else if (key == 'q') return;
+            }
+        }
+    }
+
+    class PaintballGun
+    {
+        public const int MAGAZINE_SIZE = 16;
+
+        private int balls = 0;
+        private int ballsLoaded = 0;
+
+        public int GetBallsLoaded() { return ballsLoaded;  }
+        
+        public bool IsEmpty() { return ballsLoaded == 0; }
+
+        public int Balls
+        {
+            get { return balls; }
+
+            set
+            {
+                if (value > 0)
+                    balls = value;
+                Reload();
+            }
+        }
+
+        public void Reload()
+        {
+            if (balls > MAGAZINE_SIZE)
+                ballsLoaded = MAGAZINE_SIZE;
+            else
+                ballsLoaded = balls;
+        }
+
+        public bool Shoot()
+        {
+            if (ballsLoaded == 0) return false;
+            ballsLoaded--;
+            balls--;
+            return true;
+        }
+    }
+}
